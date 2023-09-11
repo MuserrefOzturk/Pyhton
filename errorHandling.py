ad5bb640d5f9716f01feed4d5f6d3d476bd1a5dd
belindaxyz <118962940+belindaxyz@users.noreply.github.com>
2023-09-11 09:47:40 +0300

    error

---
+++ errorHandling.py
@@ -0,0 +1,55 @@
+try:
+    x=int(input('x: '))
+    y=int(input('y: '))
+    print(x/y)
+except ZeroDivisionError:
+    print('You can not enter 0 for y' )
+except ValueError:
+    print('enter digit for x and y')
+
+'''
+try:
+    x=int(input('x: '))
+    y=int(input('y: '))
+    print(x/y)
+except (ZeroDivisionError,ValueError):
+    print('You entered incorrect information' )
+'''
+'''
+try:
+    x=int(input('x: '))
+    y=int(input('y: '))
+    print(x/y)
+except:
+    print('You entered incorrect information' )
+else:
+    print('everything ok')
+'''
+'''
+while True:
+    try:
+        x=int(input('x: '))
+        y=int(input('y: '))
+        print(x/y)
+    except:    
+        print('You entered incorrect information' )
+    else:
+        break
+
+'''
+#if we want to see the error resource :
+
+
+'''
+while True:
+    try:
+        x=int(input('x: '))
+        y=int(input('y: '))
+        print(x/y)
+    except Exception as e :    
+        print('You entered incorrect information' )
+        print(e)
+    else:
+        break
+
+'''
